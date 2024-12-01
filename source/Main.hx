393e8c0187017d5b00a24b9d0afc7d83f576e5cc
ThatPeople <63274635+khuonghoanghuy@users.noreply.github.com>
2024-12-01 22:05:41 +0700

    Added Fps counter

--- source/Main.hx
+++ source/Main.hx
@@ -1,13 +1,16 @@
 package;
 
 import flixel.FlxGame;
-import openfl.display.Sprite;
+import openfl.display.*;
 
 class Main extends Sprite
 {
+	public static var fpsCounter:FPS;
 	public function new()
 	{
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
+		fpsCounter = new FPS(1, 1, 0xffffff);
+		addChild(fpsCounter);
 	}
 }
