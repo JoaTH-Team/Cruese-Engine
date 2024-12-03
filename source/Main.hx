337549a3268c23ed42c6d327992af8665a2ed443
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-12-03 21:55:11 +0700

    make sure

--- source/Main.hx
+++ source/Main.hx
@@ -23,5 +23,6 @@ class Main extends Sprite
 		addChild(new FlxGame(0, 0, GameSelectionState, 60, 60, true, false));
 		fpsCounter = new FPS(1, 1, 0xffffff);
 		addChild(fpsCounter);
+		PolyHandler.reload();
 	}
 }
